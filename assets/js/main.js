35dd6354c9ee48cc4252437963a30bbe7fcbf2af
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2021-02-07 23:43:08 +0400

    Created All web pages and written education.html, created assets, img, js, css, sass folders and written css codes

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
